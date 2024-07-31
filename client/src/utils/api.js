0f331d8951474a34b90fa47c0bd54560d5950f5a
Akash Kumar Yadav <akashkumaryadav.codesmasher@gmail.com>
2024-07-31 14:54:42 +0530

    added url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://companyreview-be.onrender.com';
 
 export const fetchCompanies = async () => {
   try {
